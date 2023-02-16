4f700e0665b4d86b317e4d4c129eb2fc8f80788a
Icaro Barros <icarodebarros@gmail.com>
2023-02-16 18:59:43 -0300

    SVGs and header icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
-import Clock from './components/clock';
+import Clock from './components/Clock';
+import StopWatch from './components/StopWatch';
+import Timer from './components/Timer';
 
 enum ModeEnum {
   Clock,
@@ -12,14 +14,29 @@ function App() {
   const [mode, setMode] = useState<ModeEnum>(ModeEnum.Clock);
 
   const getModeContent = () => {
-    switch(mode) {
-      // case ModeEnum.StopWatch: return <StopWatch />;
-      // case ModeEnum.Timer: return <Timer />;
-      default: return <Clock />;
+    switch (mode) {
+      case ModeEnum.StopWatch:
+        return <StopWatch />;
+      case ModeEnum.Timer:
+        return <Timer />;
+      default:
+        return <Clock />;
     }
-  }
+  };
 
-  return <div className="App">{getModeContent()}</div>;
+  
+
+  return (
+    <div className="App">
+      <header className="header">
+        <img src="/clock-svgrepo-com.svg" alt="Clock" className="active" />
+        <img src="/stopwatch-svgrepo-com.svg" alt="Stop_Watch" />
+        <img src="/timer-svgrepo-com.svg" alt="Timer" />
+      </header>
+
+      {getModeContent()}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/StopWatch.tsx
@@ -0,0 +1,8 @@
+export default function StopWatch() {
+
+  return (
+    <div>
+      StopWatch
+    </div>
+  );
+}
