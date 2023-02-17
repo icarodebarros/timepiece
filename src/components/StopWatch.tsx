f647091b4bfb6d56ee428b2ba0a32d5ad46ad9a3
Icaro Barros <icarodebarros@gmail.com>
2023-02-17 09:20:17 -0300

    Stopwatch play/pause button

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 export default function StopWatch() {
+  const [time, setTime] = useState<Date | undefined>();
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   return (
-    <div>
-      StopWatch
+    <div className="stopwatch-container">
+      <h1>00:00:00</h1>
+      <div className="actions">
+        <button
+          onClick={() => setIsPlaying((curValue) => !curValue)}
+          className={isPlaying ? 'pause' : 'play'}
+        ></button>
+      </div>
     </div>
   );
 }
